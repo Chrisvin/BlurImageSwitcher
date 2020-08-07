9512c75c6c3079650d43182abd7bd86bc3b5e188
Jem <chrisvin1997@gmail.com>
2020-08-08 05:10:22 +0530

    Add BlurImageSwitcher

---
+++ BlurImageSwitcher/src/main/java/com/jem/blurimageswitcher/BlurImageSwitcher.kt
@@ -0,0 +1,13 @@
+package com.jem.blurimageswitcher
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageSwitcher
+
+class BlurImageSwitcher: ImageSwitcher {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+
+
+}
