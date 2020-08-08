15211e8a7ef72872fd5e73e3c94dc2d239527236
Jem <chrisvin1997@gmail.com>
2020-08-09 04:15:03 +0530

    Add BlurAnimation

---
+++ BlurImageSwitcher/src/main/java/com/jem/blurimageswitcher/BlurAnimation.kt
@@ -0,0 +1,51 @@
+package com.jem.blurimageswitcher
+
+import android.graphics.Bitmap
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.widget.ImageView
+
+class BlurAnimation constructor(
+    private val imageView: ImageView,
+    private val bitmap: Bitmap,
+    startValue: Int,
+    stopValue: Int
+) : Animation() {
+    private val startValue: Float = startValue.toFloat()
+    private val stopValue: Float = stopValue.toFloat()
+    private val difValue: Float = stopValue - startValue.toFloat()
+
+    override fun applyTransformation(
+        interpolatedTime: Float,
+        t: Transformation?
+    ) {
+        super.applyTransformation(interpolatedTime, t)
+        if (interpolatedTime > 0.98) {
+            // In the final animation frames, reset to the original image,
+            // so that it can be used for future blur animations
+            imageView.setImageBitmap(bitmap)
+        } else {
+            val current = (difValue * interpolatedTime + startValue + 0.5f).toInt()
+            val blurred = BlurUtil.scaledBitmapBlur(bitmap, current)
+            imageView.setImageBitmap(blurred)
+        }
+        // Stop value being greater than start value indicates that it's a view going out animation
+        imageView.alpha = if (stopValue - startValue > 0) {
+            // Fade out
+            1f - interpolatedTime
+        } else {
+            // Fade in
+            interpolatedTime
+        }
+        val scaleFactor = if (stopValue - startValue > 0) {
+            // Scale out
+            1f + interpolatedTime * 0.1f
+        } else {
+            // Scale in
+            1.1f - interpolatedTime * 0.1f
+        }
+        imageView.scaleX = scaleFactor
+        imageView.scaleY = scaleFactor
+    }
+
+}
