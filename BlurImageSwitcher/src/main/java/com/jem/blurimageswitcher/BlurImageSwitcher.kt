57de83ec5a5f35bc3b0d91e3bbc240f4f31dc017
Jem <chrisvin1997@gmail.com>
2020-08-09 05:17:10 +0530

    Add blur animation in BlurImageSwitcher

--- BlurImageSwitcher/src/main/java/com/jem/blurimageswitcher/BlurImageSwitcher.kt
+++ BlurImageSwitcher/src/main/java/com/jem/blurimageswitcher/BlurImageSwitcher.kt
@@ -3,11 +3,33 @@ package com.jem.blurimageswitcher
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageSwitcher
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
 
-class BlurImageSwitcher: ImageSwitcher {
+
+class BlurImageSwitcher : ImageSwitcher {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
+    override fun showNext() {
+        val nextImageView = nextView as ImageView?
+        val nextDrawable = nextImageView?.drawable?.toBitmap()
+        if (nextImageView != null && nextDrawable != null) {
+            val inBlurAnimation = BlurAnimation(nextImageView, nextDrawable, 100, 0)
+            inBlurAnimation.duration =
+                resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+            inAnimation = inBlurAnimation
+        }
 
+        val currentImageView = currentView as ImageView?
+        val currentDrawable = currentImageView?.drawable?.toBitmap()
+        if (currentImageView != null && currentDrawable != null) {
+            val outBlurAnimation = BlurAnimation(currentImageView, currentDrawable, 0, 100)
+            outBlurAnimation.duration =
+                resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+            outAnimation = outBlurAnimation
+        }
 
+        super.showNext()
+    }
 }
