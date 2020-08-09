5153b6d89776b29ffcff5c02aa5ad306516e23cf
Jem <chrisvin1997@gmail.com>
2020-08-09 16:09:20 +0530

    Add spinner for user selected images in demo app
    Also add button to start/stop auto image changes

--- app/src/main/java/com/jem/blurimageswitcherdemo/MainActivity.kt
+++ app/src/main/java/com/jem/blurimageswitcherdemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.jem.blurimageswitcherdemo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -8,7 +11,9 @@ import kotlin.concurrent.scheduleAtFixedRate
 
 class MainActivity : AppCompatActivity() {
     private var imageIndex = 0
-    private lateinit var timer: Timer
+    private var timer: Timer? = null
+
+    private var isUserInteracting = false
 
     private val imageResourceList = arrayListOf(
         R.drawable.ic_undraw_black_lives_matter_rndk,
@@ -18,16 +23,45 @@ class MainActivity : AppCompatActivity() {
         R.drawable.ic_undraw_smiley_face_lmgm
     )
 
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        isUserInteracting = true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        timer = Timer()
-        timer.scheduleAtFixedRate(1000, 1500) {
-            runOnUiThread {
-//                    imageSwitcher.showNext()
-                imageSwitcher.setImageResource(imageResourceList[imageIndex])
-                imageIndex = (imageIndex + 1) % 5
+        imageSpinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            arrayListOf("Image 1", "Image 2", "Image 3", "Image 4", "Image 5")
+        )
+        imageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // Don't care
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (isUserInteracting) {
+                    imageSwitcher.setImageResource(imageResourceList[position])
+                }
+            }
+        }
+
+        startAutoChange()
+        autoChangeButton.setOnClickListener {
+            if (timer == null) {
+                startAutoChange()
+                autoChangeButton.text = "Stop autochange"
+            } else {
+                stopAutoChange()
+                autoChangeButton.text = "Start autochange"
             }
         }
 
@@ -38,5 +72,21 @@ class MainActivity : AppCompatActivity() {
 //            this.resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
 //        currentImageView.animation = outBlurAnimation
 //        currentImageView.animate()
+    }
+
+    private fun startAutoChange() {
+        timer = Timer()
+        timer?.scheduleAtFixedRate(1000, 1500) {
+            runOnUiThread {
+//                    imageSwitcher.showNext()
+                imageSwitcher.setImageResource(imageResourceList[imageIndex])
+                imageIndex = (imageIndex + 1) % 5
+            }
+        }
+    }
+
+    private fun stopAutoChange() {
+        timer?.cancel()
+        timer = null
     }
 }
